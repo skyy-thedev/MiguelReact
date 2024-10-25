e3b8ee5163bd3e29cc28c92652fadb0559a548c5
Skyy the dev <skyzeduardo@gmail.com>
2024-10-25 13:41:18 -0300

    create header component

---
+++ migueliugas/src/components/header.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useRef, useState } from 'react';
+import logo from '../assets/header/logo.png';
+import '../components/styles/header.css';
+import Wapp from '../assets/icons/wapp.svg';
+import Insta from '../assets/icons/instagram.svg';
+import Face from '../assets/icons/facebook.svg';
+
+const Header = () => {
+  const contatoBtnRef = useRef(null);
+  const socialIconsRef = useRef(null);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const contatoBtn = contatoBtnRef.current;
+    const socialIcons = socialIconsRef.current;
+
+    const handleContatoClick = () => {
+      socialIcons.classList.toggle('visible');
+      socialIcons.classList.toggle('hidden');
+    };
+
+    if (contatoBtn) {
+      contatoBtn.addEventListener('click', handleContatoClick);
+    }
+
+    return () => {
+      if (contatoBtn) {
+        contatoBtn.removeEventListener('click', handleContatoClick);
+      }
+    };
+  }, []);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <header className="header">
+      <div className="ContainerFlex">
+        <a href="/"><img src={logo} alt="Logo" id="logo" /></a>
+        <div className="menu-toggle" onClick={toggleMenu}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <nav className={menuOpen ? 'open' : ''}>
+          <ul>
+            <li><a href="#sobre">Sobre</a></li>
+            <li><a href="#localizacao">Localização</a></li>
+            <li><a href="https://hotmart.com/pt-br/marketplace/produtos?initialSelectedCategory=&q=Miguel+Iugas">Mentorias e Cursos</a></li>
+            <li><a href="#services">Agendamento</a></li>
+
+            <li>
+              <button id='contato-btn' ref={contatoBtnRef}>
+                Contato
+              </button>
+            </li>
+            <div id="social-icons" ref={socialIconsRef} className="hidden">
+              <a href="https://api.whatsapp.com/send?phone=5511933058210&text=Ol%C3%A1%21%20Eu%20gostaria%20de%20agendar%20um%20atendimento%20com%20Dr.%20Miguel%20Iugas%20%5B2652%5D" className="whatsapp-icon" target="_blank" rel="noopener noreferrer" title="whatsapp">
+                <img src={Wapp} alt="WhatsApp" id="whatsapp-icon"/>
+              </a>
+              <a href="https://www.instagram.com/migueliugas/" className="instagram-icon" target="_blank" rel="noopener noreferrer" title="instagram">
+                <img src={Insta} alt="Instagram" id="instagram-icon"/>
+              </a>
+              <a href="https://www.facebook.com/migueliugasfarmaceutico" className="facebook-icon" target="_blank" rel="noopener noreferrer" title="facebook">
+                <img src={Face} alt="Facebook" id="facebook-icon"/>
+              </a>
+            </div>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ migueliugas/src/constants/theme.js
@@ -0,0 +1,20 @@
+const theme = {
+    COLORS: {
+        strongblue: "#022031",
+        mediumblue: "#297D94",
+        medlgblue: "#06A1B4",
+        lightblue: "#17EAD9",
+        gray1: "#323434",
+        white: "#ffffff",
+    },
+    FONT_SIZE: {
+        xsm: 11,
+        sm: 14,
+        md: 16,
+        lg: 18,
+        xl: 32,
+        xxl: 64
+    }
+};
+
+export default theme;
