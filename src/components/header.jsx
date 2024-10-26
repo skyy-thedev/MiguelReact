633a5561becf7fb9a81e3cf61b5afdddafdbe0dc
Skyy the dev <skyzeduardo@gmail.com>
2024-10-26 17:04:50 -0300

    atualização geral

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,6 +8,8 @@ import Face from '../assets/icons/facebook.svg';
 const Header = () => {
   const contatoBtnRef = useRef(null);
   const socialIconsRef = useRef(null);
+  const [submenuOpen, setSubmenuOpen] = useState(false);
+  const submenuRef = useRef(null);
   const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
@@ -34,6 +36,10 @@ const Header = () => {
     setMenuOpen(!menuOpen);
   };
 
+  const handleSubmenuToggle = () => {
+    setSubmenuOpen(!submenuOpen);
+  }; 
+
   return (
     <header className="header">
       <div className="ContainerFlex">
@@ -48,13 +54,17 @@ const Header = () => {
             <li><a href="/Login">Login</a></li>
             <li><a href="#Sobre">Sobre</a></li>
             <li><a href="#Localizacao">Localização</a></li>
-            <li><a href="https://hotmart.com/pt-br/marketplace/produtos?initialSelectedCategory=&q=Miguel+Iugas">Mentorias e Cursos</a></li>
-            <li><a href="#services">Agendamento</a></li>
-
             <li>
-              <button id='contato-btn' ref={contatoBtnRef}>
-                Contato
-              </button>
+              <a onClick={handleSubmenuToggle} href="#mentorias">Mentorias e Cursos</a>
+              <ul ref={submenuRef} className={`submenu ${submenuOpen ? 'open' : 'closed'}`}>
+                  <li><a href="https://hotmart.com/pt-br/marketplace/produtos/suplementacao-endovenosa-e-intramuscular">Mentoria em Suplementação Endovenosa e Intramuscular</a></li>
+                  <li><a href="https://hotmart.com/pt-br/marketplace/produtos/prescricoes">Prescrições na Saúde Estética</a></li>
+                  <li><a href="https://hotmart.com/pt-br/marketplace/produtos?initialSelectedCategory=&q=Miguel+Iugas">Ver todos</a></li>
+              </ul>
+            </li>
+            <li><a href="#services">Agendamento</a></li>
+            <li>
+              <button id='contato-btn' ref={contatoBtnRef}>Contato</button>
             </li>
             <div id="social-icons" ref={socialIconsRef} className="hidden">
               <a href="https://api.whatsapp.com/send?phone=5511933058210&text=Ol%C3%A1%21%20Eu%20gostaria%20de%20agendar%20um%20atendimento%20com%20Dr.%20Miguel%20Iugas%20%5B2652%5D" className="whatsapp-icon" target="_blank" rel="noopener noreferrer" title="whatsapp">
--- src/components/heroSlider.jsx
+++ src/components/heroSlider.jsx
@@ -9,7 +9,7 @@ import './styles/heroSlider.css';
 const flickityOptions = {
     wrapAround: true,
     autoPlay: 6000,
-    pauseAutoPlayOnHover: false,
+    pauseAutoPlayOnHover: true,
     prevNextButtons: true,
     contain: true, 
     initialIndex: 1
@@ -19,7 +19,7 @@ const HeroSlider = () => {
     const slides = [
         { image: Consultoria, header: "Agendamento", text: "Consultoria", link: "https://api.whatsapp.com/send?phone=5511933058210&text=Ol%C3%A1%21%20Eu%20gostaria%20de%20agendar%20um%20atendimento%20com%20Dr.%20Miguel%20Iugas%20%5B2652%5D", buttonText: "Saiba mais!" },
         { image: Agenda, header: "Lives e Palestras", text: "Agenda", link: "https://www.instagram.com/migueliugas/", buttonText: "Calendário!", isAgenda: true },
-        { image: Aulas, header: "Mentorias e Cursos", text: "Aulas", link: "https://www.instagram.com/migueliugas/", buttonText: "Aprender mais!" },
+        { image: Aulas, header: "Mentorias e Cursos", text: "Aulas", link: "https://hotmart.com/pt-br/marketplace/produtos?initialSelectedCategory=&q=Miguel+Iugas", buttonText: "Aprender mais!" },
     ];
 
     return (
@@ -27,11 +27,11 @@ const HeroSlider = () => {
             {slides.map((slide, index) => (
                 <div
                     key={index}
-                    className={`carousel-cell ${slide.isAgenda ? 'agenda-slide' : ''}`}
+                    className={`carousel-cell `}
                     style={{ backgroundImage: `url(${slide.image})`, backgroundColor: '#f4f4f4', backgroundSize: 'cover', backgroundPosition: 'center' }}
                 >
                     <div className="info-container">
-                        {slide.header && <h3 className="header">{slide.header}</h3>}
+                        <h3 className="header">{slide.header}</h3>
                         <h2 className="text">{slide.text}</h2>
                         <a href={slide.link} target="_blank" rel="noreferrer">
                             <button>{slide.buttonText}</button>
