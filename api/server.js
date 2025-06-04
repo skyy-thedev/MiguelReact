c77bd0c7e2ae1b5071b569c7aacc605002002f39
Skyy the dev <skyzeduardo@gmail.com>
2025-06-04 15:29:13 -0300

    PORT alt

--- api/server.js
+++ api/server.js
@@ -30,7 +30,7 @@ mongoose.connect(process.env.MONGO_URI, {
 .catch(err => console.error('Erro ao conectar ao MongoDB:', err));
 
 // Iniciar o servidor
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
