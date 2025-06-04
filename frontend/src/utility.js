6cf23090fff27559d857ad0b0fa9fcccef654089
Skyy the dev <skyzeduardo@gmail.com>
2025-06-04 13:58:42 -0300

    2.1 version launched

--- frontend/src/utility.js
+++
@@ -1,7 +0,0 @@
-// src/utils/api.js
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000';
-
-export const fetchAgendamentos = async () => {
-  const response = await fetch(`${API_BASE_URL}/api/agendamentos/agendamentos`);
-  return response.json();
-};
